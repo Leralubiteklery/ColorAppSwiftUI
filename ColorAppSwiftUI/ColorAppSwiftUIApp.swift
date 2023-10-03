799a5ce4f3d3afa22e320013dcc6776e7f169b8a
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2023-10-03 15:49:54 +0300

    Initial Commit

---
+++ ColorAppSwiftUI/ColorAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorAppSwiftUIApp.swift
+//  ColorAppSwiftUI
+//
+//  Created by Lera Savchenko on 3.10.23.
+//
+
+import SwiftUI
+
+@main
+struct ColorAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
