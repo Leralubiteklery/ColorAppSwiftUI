b90d36f51476f99affe2c3a7081f81ae6c49c3ee
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2023-10-04 16:39:41 +0300

    Added colorSliders.

--- ColorAppSwiftUI/ContentView.swift
+++ ColorAppSwiftUI/ContentView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var redSliderValue = Double.random(in: 0...255)
+    @State private var greenSliderValue = Double.random(in: 0...255)
+    @State private var blueSliderValue = Double.random(in: 0...255)
+    
     var body: some View {
         ZStack(alignment: .top) {
                 Color.indigo
@@ -18,13 +22,14 @@ struct ContentView: View {
                     .frame(width: 350, height: 170)
                     .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.white, lineWidth: 4))
                     .padding()
-
-                
-                    
+                ColorSliderView(value: $redSliderValue, sliderColor: .red)
+                ColorSliderView(value: $greenSliderValue, sliderColor: .green)
+                ColorSliderView(value: $blueSliderValue, sliderColor: .blue)
             }
-
-            Spacer()
+            .padding()
+            
         }
+        
  
     }
 
@@ -33,5 +38,18 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ColorSliderView: View {
+    @Binding var value: Double
+    let sliderColor: Color
+    
+    var body: some View {
+        HStack{
+            Text("\(lround(value))")
+            Slider(value: $value, in: 0...255, step: 1)
+                .accentColor(sliderColor)
+        }
     }
 }
